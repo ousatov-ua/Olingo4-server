708b832be08db4f5fd3777f5e1e2d12461bfc64d
Oleksii Usatov <usatovalexey81@gmail.com>
2023-07-25 15:19:20 +0300

    Reformat.

--- src/test/resources/ddl/PropertyDataTax.sql
+++ src/test/resources/ddl/PropertyDataTax.sql
@@ -1,9 +1,8 @@
 create table PropertyDataTax
 (
-    ListingId            int unsigned not null
-        primary key,
-    ParcelNumber         varchar(50) null,
-    TaxAnnualAmount      int null,
-    TaxSpecialAssessment int null
+    ListingId            int unsigned not null primary key,
+    ParcelNumber         varchar(50)  null,
+    TaxAnnualAmount      int          null,
+    TaxSpecialAssessment int          null
 );
 
