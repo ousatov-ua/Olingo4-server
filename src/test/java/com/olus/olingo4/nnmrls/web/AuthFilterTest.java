5b8ae8317eaa64841ebc51957cc3e1ac99864b55
Oleksii Usatov <usatovalexey81@gmail.com>
2023-08-05 11:34:06 +0300

    Added test coverage, did optimizations.

--- src/test/java/com/olus/olingo4/nnmrls/web/AuthFilterTest.java
+++ src/test/java/com/olus/olingo4/nnmrls/web/AuthFilterTest.java
@@ -20,6 +20,11 @@ import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+/**
+ * Unit tests for {@link AuthFilter}
+ *
+ * @author Oleksii Usatov
+ */
 @ExtendWith(MockitoExtension.class)
 class AuthFilterTest {
 
